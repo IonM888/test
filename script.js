6e8c588fb67fc29464a37e97855c843f2aa0df16
I-M <i.mustafenco@gmail.com>
2021-02-09 12:25:12 +0200

    s-a unit html cu css, sa modificat stilul patratului in css, s-a creat un buton si o functie de creare buton in js

---
+++ script.js
@@ -0,0 +1,16 @@
+let body= document.querySelector('body')
+const button= document.createElement("button")
+
+button.textContent="Creaza un patrat"
+
+body.appendChild(button)
+
+
+const create= ()=>{
+    const patrat= document.createElement("div")
+    patrat.classList.add("patrat")
+    body.appendChild(patrat)
+}
+
+
+button.addEventListener("click",create)
